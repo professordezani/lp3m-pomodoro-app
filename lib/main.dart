1aa953576967f3cd2a796a376507a342feb5406c
Dezani <henrique.dezani@fatecriopreto.edu.br.local>
2026-03-10 08:55:46 -0300

    Tela finalizada

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ class PomodoroApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Material(
         color: Colors.green[50],
         child: Column(
@@ -36,6 +37,55 @@ class PomodoroApp extends StatelessWidget {
               fontWeight: FontWeight.w200,
               height: 0.85
             )),
+           Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            spacing: 16,
+            children: [
+              InkWell(
+                onTap: () {},
+                borderRadius: BorderRadius.circular(32),
+                splashColor: Colors.green[100],
+                child: Container(
+                  width: 80,
+                  height: 80,
+                  decoration: BoxDecoration(
+                    color: Color.fromRGBO(77, 218, 110, 0.15),
+                    borderRadius: .circular(32),
+                  ),
+                  child: Icon(Icons.forward_10_outlined, size: 32)
+                ),
+              ),
+              InkWell(
+                onTap: () {},
+                borderRadius: BorderRadius.circular(32),
+                splashColor: Colors.green[300],
+                child: Container(
+                  width: 102,
+                  height: 96,
+                  decoration: BoxDecoration(
+                    color: Color.fromRGBO(77, 218, 110, 0.62),
+                    borderRadius: .circular(32),
+                  ),
+                  child: Icon(Icons.play_arrow, size: 32)
+                ),
+              ),
+              InkWell(
+                onTap: () {},
+                borderRadius: BorderRadius.circular(32),
+                splashColor: Colors.green[100],
+                child: Container(
+                  width: 80,
+                  height: 80,
+                  decoration: BoxDecoration(
+                    color: Color.fromRGBO(77, 218, 110, 0.15),
+                    borderRadius: .circular(32),
+                  ),
+                  child: Icon(Icons.fast_forward, size: 32)
+                ),
+              ),
+            ],
+           ), 
           ]
         ),
       ),
