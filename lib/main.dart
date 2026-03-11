e27054f17dbb8a8f4573d1f9ef8b902e8e37ac75
Dezani <henrique.dezani@fatecriopreto.edu.br.local>
2026-03-11 07:59:35 -0300

    Altera literais por variáveis

--- lib/main.dart
+++ lib/main.dart
@@ -5,12 +5,21 @@ void main() {
 }
 
 class PomodoroApp extends StatelessWidget {
+  // atributos:
+  var backgroundColor = Colors.green[50];
+  var borderColor = Colors.green;
+  var textColor = Colors.green[900];
+  var smallSplashColor = Colors.green[100];
+  var bigSplashColor = Colors.green[300];
+  var lightGreenColor = Color.fromRGBO(77, 218, 110, 0.15);
+  var darkGreenColor = Color.fromRGBO(77, 218, 110, 0.62);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Material(
-        color: Colors.green[50],
+        color: backgroundColor,
         child: Column(
           spacing: 32,
           mainAxisAlignment: MainAxisAlignment.center,
@@ -19,74 +28,77 @@ class PomodoroApp extends StatelessWidget {
               width: 125,
               padding: EdgeInsets.all(8),
               decoration: BoxDecoration(
-                color: Color.fromRGBO(77, 218, 110, 0.15),
-                border: Border.all(color: Colors.green),
+                color: lightGreenColor,
+                border: Border.all(color: borderColor),
                 borderRadius: BorderRadius.circular(25),
               ),
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
                   Icon(Icons.coffee_outlined, size: 18),
-                  Text("Short Break")
+                  Text("Short Break"),
                 ],
               ),
             ),
-            Text("05\n00", style: TextStyle(
-              color: Colors.green[900],
-              fontSize: 256,
-              fontWeight: FontWeight.w200,
-              height: 0.85
-            )),
-           Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            spacing: 16,
-            children: [
-              InkWell(
-                onTap: () {},
-                borderRadius: BorderRadius.circular(32),
-                splashColor: Colors.green[100],
-                child: Container(
-                  width: 80,
-                  height: 80,
-                  decoration: BoxDecoration(
-                    color: Color.fromRGBO(77, 218, 110, 0.15),
-                    borderRadius: .circular(32),
-                  ),
-                  child: Icon(Icons.forward_10_outlined, size: 32)
-                ),
+            Text(
+              "05\n00",
+              style: TextStyle(
+                color: textColor,
+                fontSize: 256,
+                fontWeight: FontWeight.w200,
+                height: 0.85,
               ),
-              InkWell(
-                onTap: () {},
-                borderRadius: BorderRadius.circular(32),
-                splashColor: Colors.green[300],
-                child: Container(
-                  width: 102,
-                  height: 96,
-                  decoration: BoxDecoration(
-                    color: Color.fromRGBO(77, 218, 110, 0.62),
-                    borderRadius: .circular(32),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              spacing: 16,
+              children: [
+                InkWell(
+                  onTap: () {},
+                  borderRadius: BorderRadius.circular(32),
+                  splashColor: smallSplashColor,
+                  child: Container(
+                    width: 80,
+                    height: 80,
+                    decoration: BoxDecoration(
+                      color: lightGreenColor,
+                      borderRadius: .circular(32),
+                    ),
+                    child: Icon(Icons.forward_10_outlined, size: 32),
                   ),
-                  child: Icon(Icons.play_arrow, size: 32)
                 ),
-              ),
-              InkWell(
-                onTap: () {},
-                borderRadius: BorderRadius.circular(32),
-                splashColor: Colors.green[100],
-                child: Container(
-                  width: 80,
-                  height: 80,
-                  decoration: BoxDecoration(
-                    color: Color.fromRGBO(77, 218, 110, 0.15),
-                    borderRadius: .circular(32),
+                InkWell(
+                  onTap: () {},
+                  borderRadius: BorderRadius.circular(32),
+                  splashColor: bigSplashColor,
+                  child: Container(
+                    width: 102,
+                    height: 96,
+                    decoration: BoxDecoration(
+                      color: darkGreenColor,
+                      borderRadius: .circular(32),
+                    ),
+                    child: Icon(Icons.play_arrow, size: 32),
                   ),
-                  child: Icon(Icons.fast_forward, size: 32)
                 ),
-              ),
-            ],
-           ), 
-          ]
+                InkWell(
+                  onTap: () {},
+                  borderRadius: BorderRadius.circular(32),
+                  splashColor: smallSplashColor,
+                  child: Container(
+                    width: 80,
+                    height: 80,
+                    decoration: BoxDecoration(
+                      color: lightGreenColor,
+                      borderRadius: .circular(32),
+                    ),
+                    child: Icon(Icons.fast_forward, size: 32),
+                  ),
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
